fab304faf686e6a0ab583d8984dad2e3fc0b480f
Stan Skrivanek <s.skrivanek@icloud.com>
2021-11-28 23:26:37 +0000

    ADD: New JS approach in script-2

--- script-2.js
+++ script-2.js
@@ -5,27 +5,33 @@ const statColleagues = document.querySelector("#stat-colleagues");
 const statDays = document.querySelector("#stat-days");
 const statCosts = document.querySelector("#stat-cost");
 
+const minp = range.min ? range.min : 0;
+const maxp = range.max ? range.max : 100;
+
+let ctp = {
+  current: 0,
+};
+
+const currentPosition = () => {
+  ctp.current = range.value;
+};
+
 // logarithmic increase
 function initLogValue() {
-  const startingPosition = range.value;
-  const minp = range.min ? range.min : 0; // input min attribute Value => steps
-  const maxp = range.max ? range.max : 100; // input max attribute Value => steps
   const minv = Math.log(10); // min Value - minEmployees
   const maxv = Math.log(100000); // max Value - maxEmployees
   const scale = (maxv - minv) / (maxp - minp);
-  const value = Math.exp(minv + scale * (startingPosition - minp));
+  const value = Math.exp(minv + scale * (ctp.current - minp));
   const rounded = Math.floor(value);
+  console.log(value);
   return rounded;
 }
 
 // set bubble position based on slide thumb position to be aligned with thumb and display employees number
-function setBubble(range, bubble, value) {
-  const val = range.value;
-  const minp = range.min ? range.min : 0;
-  const maxp = range.max ? range.max : 100;
-  const newVal = Number(((val - minp) * 100) / (maxp - minp));
+function setBubble(nwc) {
+  const newVal = Number(((ctp.current - minp) * 100) / (maxp - minp));
   // rendeer employees number in thumb bubble
-  bubble.innerHTML = value;
+  bubble.innerHTML = nwc;
   // Set bubble position - Sorta magic numbers based on size of the native UI thumb
   bubble.style.left = `calc(${newVal}% + (${36 - newVal * 0.7}px))`;
 }
@@ -40,11 +46,12 @@ function updateStats() {
   statColleagues.innerHTML = numberWithCommas(colleagues);
   statDays.innerHTML = numberWithCommas(days);
   statCosts.innerHTML = numberWithCommas(`€ ${cost}`);
-
-  setBubble(range, bubble, nwc);
+  
+  setBubble(nwc);
 }
 
 range.addEventListener("input", () => {
+  currentPosition();
   updateStats();
 });
 
@@ -64,4 +71,3 @@ for (let e of document.querySelectorAll(
   e.style.setProperty("--max", e.max == "" ? "100" : e.max);
   e.addEventListener("input", () => e.style.setProperty("--value", e.value));
 }
-
